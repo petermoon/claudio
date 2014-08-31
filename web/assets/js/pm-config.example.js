6d4678153f7a7d5ef56adbc4b69d0b73b7d475ed
petermoon <peter.moon00@gmail.com>
2014-08-30 17:13:44 -0700

    initial commit

---
+++ web/assets/js/pm-config.example.js
@@ -0,0 +1,7 @@
+var pmConfig = {
+    accessKeyId: 'YOUR_ACCESS_KEY_ID',
+    secretAccessKey: 'YOUR_SECRET_ACCESS_KEY',
+    roleArn: 'arn:aws:iam::YOUR_ACCOUNT_NUMBER:role/claudio_anonymous',
+    bucket: 'YOUR_MUSIC_BUCKET',
+    region: 'YOUR_REGION'
+};
