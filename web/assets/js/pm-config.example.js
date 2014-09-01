6a4514cce15ca43f3a2b749ce54e2f5cefced30d
petermoon <peter.moon00@gmail.com>
2014-09-01 01:49:57 -0700

    Update auth mechanism to Facebook with web identity federation

--- web/assets/js/pm-config.example.js
+++ web/assets/js/pm-config.example.js
@@ -1,7 +1,6 @@
 var pmConfig = {
-    accessKeyId: 'YOUR_ACCESS_KEY_ID',
-    secretAccessKey: 'YOUR_SECRET_ACCESS_KEY',
-    roleArn: 'arn:aws:iam::YOUR_ACCOUNT_NUMBER:role/claudio_anonymous',
+    roleArn: 'YOUR_WEB_IDENTITY_ROLE_ARN',
     bucket: 'YOUR_MUSIC_BUCKET',
-    region: 'YOUR_REGION'
+    region: 'YOUR_REGION',
+    facebookAppId: 'YOUR_FACEBOOK_APP_ID'
 };
