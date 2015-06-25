46d30fdbdd88cd835ff2bf443dae78e61ae93d9f
petermoon <peter.moon00@gmail.com>
2015-06-24 23:43:32 -0700

    change auth to Cognito

--- web/assets/js/claudio-config.example.js
+++ web/assets/js/claudio-config.example.js
@@ -1,6 +1,7 @@
 var pmConfig = {
-    roleArn: 'YOUR_WEB_IDENTITY_ROLE_ARN',
     bucket: 'YOUR_MUSIC_BUCKET',
-    region: 'YOUR_REGION',
-    facebookAppId: 'YOUR_FACEBOOK_APP_ID'
+    region: 'YOUR_MUSIC_BUCKET_REGION',
+    cognito_id_pool_id: 'YOUR_COGNITO_IDENTITY_POOL_ID',
+    cognito_region: 'YOUR_COGNITO_REGION',
+    facebook_app_id: 'YOUR_FACEBOOK_APP_ID'
 };
